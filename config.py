85d31a30c04fe7d9905d82c289204f2f7bf3938c
Zach Cheung <953923621@qq.com>
2023-09-04 20:35:06 +0800

    新增日志路径

--- config.py
+++ config.py
@@ -1,14 +1,16 @@
-data_file_name = "data.csv"
+data_file_name = 'data.csv'
 
 name_path = {
-    "ssq": {
-        "name": "双色球",
-        "path": "data/ssq/",
-        "analysis": "analysis/ssq/"
+    'ssq': {
+        'name': '双色球',
+        'path': 'data/ssq/',
+        'analysis': 'analysis/ssq/',
+        'log_file': 'log/ssq_log_file.log'
     },
-    "dlt": {
-        "name": "大乐透",
-        "path": "data/dlt/",
-        "analysis": "analysis/dlt/"
+    'dlt': {
+        'name': '大乐透',
+        'path': 'data/dlt/',
+        'analysis': 'analysis/dlt/',
+        'log_file': 'log/dlt_log_file.log'
     }
 }
