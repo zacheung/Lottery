fc0a304401b988e000d180700a8185e49bf27806
Zach Cheung <953923621@qq.com>
2023-09-05 23:43:14 +0800

    新增模型训练参数

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
+# 数据文件名
 data_file_name = 'data.csv'
 
+# 训练列
+train_columns = ['red_ball1', 'red_ball2', 'red_ball3', 'red_ball4', 'red_ball5', 'red_ball6', 'blue_ball']
+
+# 路径配置
 name_path = {
     'ssq': {
         'name': '双色球',
@@ -12,5 +17,13 @@ name_path = {
         'path': 'data/dlt/',
         'analysis': 'analysis/dlt/',
         'log_file': 'log/dlt_log_file.log'
+    }
+}
+
+model_args = {
+    'ssq': {
+        'model_args': {
+            'windows_size': 3
+        }
     }
 }
